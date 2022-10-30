1022ae0af2b69b318f165a913fcaec1cef3678cb
Kwon Valerii <wellko24@gmail.com>
2022-10-31 01:21:25 +0600

    does App.tsx and added some css Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState}  from 'react';
 import './App.css';
+import Board from "./components/Board/Board";
+import Counter from "./components/Counter/counter";
+import Reset from "./components/Reset/Reset";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let countNumber = 0;
+
+	const [count, setCount] = useState(countNumber);
+
+	const createItems = () => {
+		const result = [];
+		const random = Math.floor(Math.random() * 36)
+		for (let i = 0; i < 36; i++) {
+			if (i === random) {
+				result.push({hasItem: true, clicked: false, id: i, click: ()=> open(i)})
+			} else {
+				result.push({hasItem: false, clicked: false, id: i, click: ()=> open(i)})
+			}
+		}
+		return result;
+	}
+
+	const [items, setItems] = useState(createItems());
+
+
+	const resetAction = () =>{
+		setItems(createItems);
+		countNumber = 0;
+		setCount(countNumber);
+		console.log(items)
+	}
+
+	const open = (index:number) => {
+		let itemsCopy = [...items];
+		const rightItem = itemsCopy.find(item => item.id === index)!
+		if (!itemsCopy[itemsCopy.indexOf(rightItem)].clicked){
+			itemsCopy[itemsCopy.indexOf(rightItem)].clicked = true;
+			setItems(itemsCopy);
+			countNumber++;
+			setCount(countNumber);
+			if (itemsCopy[itemsCopy.indexOf(rightItem)].hasItem){
+				alert('Win!')
+				for (let i = 0; i < 36; i++){
+					itemsCopy[i].clicked = true;
+					setItems(itemsCopy);
+				}
+			}
+		}
+	}
+
+	return (
+		<div className="App">
+			<Board cells={items}/>
+			<Counter counter={count} />
+			<Reset action={resetAction}/>
+		</div>
+	);
 }
 
 export default App;
